f3746fe22d7bb23c1be0e65530ef53725a744234
Bryan Gomes <bryanpgomes@gmail.com>
2020-05-18 02:37:43 +0000

    solution to smart parking

---
+++ Kata6.js
@@ -0,0 +1,53 @@
+const whereCanIPark = function (spots, vehicle) {
+  // Code here!
+  let parkingSpot = false;
+    for (let row = 0; row < spots.length; row++) {
+      for (let column = 0; column < spots[row].length; column++){
+        let spot = spots[row][column]
+        if ((vehicle === "regular") && (spot === "R")) {
+          return [column, row];
+        } else if ((vehicle === "small") && (spot === "R" || spot === "S")) {
+          return [column, row];
+        } else if ((vehicle === 'motorcycle') && (spot === "R" || spot === "S" || spot === "M")) {
+          return [column, row];
+       }
+      }
+    }
+  return parkingSpot;
+};
+
+console.log(whereCanIPark(
+  [
+    // COLUMNS ARE X
+    // 0    1    2    3    4    5
+    ['s', 's', 's', 'S', 'R', 'M'], // 0 ROWS ARE Y
+    ['s', 'M', 's', 'S', 'r', 'M'], // 1
+    ['s', 'M', 's', 'S', 'r', 'm'], // 2
+    ['S', 'r', 's', 'm', 'r', 'M'], // 3
+    ['S', 'r', 's', 'm', 'r', 'M'], // 4
+    ['S', 'r', 'S', 'M', 'M', 'S']  // 5
+  ],
+  'regular'
+));
+
+console.log(whereCanIPark(
+  [
+    ['M', 'M', 'M', 'M'],
+    ['M', 's', 'M', 'M'],
+    ['M', 'M', 'M', 'M'],
+    ['M', 'M', 'r', 'M']
+  ],
+  'small'
+));
+
+console.log(whereCanIPark(
+  [
+    ['s', 's', 's', 's', 's', 's'],
+    ['s', 'm', 's', 'S', 'r', 's'],
+    ['s', 'm', 's', 'S', 'r', 's'],
+    ['S', 'r', 's', 'm', 'r', 's'],
+    ['S', 'r', 's', 'm', 'R', 's'],
+    ['S', 'r', 'S', 'M', 'm', 'S']
+  ],
+  'motorcycle'
+))
